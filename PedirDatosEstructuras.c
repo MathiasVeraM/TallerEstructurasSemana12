048f90a1da6718d356d3b75e46e49be753ae706e
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-06-19 15:39:00 -0500

    Primera version, prueba de funcion gets y puts, ademas de añadir la definicion de las funciones

---
+++ PedirDatosEstructuras.c
@@ -0,0 +1,22 @@
+/*Crear un programa que pida datos al usuario, usando estructuras y funciones gets() y puts(), puede ser tambien usando la variante scanf() y consultar fflush()
+gets: Función que se encarga de leer y almacenar una cadena de caracteres introducida mediante el teclado. La variable es guardada hasta que haya un salto de línea (“\n”)
+puts: Función que se encarga de mostrar (Dato de salida) una cadena de caracteres especifica.
+fflush: realiza la limpieza del buffer de entrada*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> //Para usar gets y puts
+int main(){
+    struct alumno{
+        char nombre[20];
+        char direccion[20];
+        char carrera[20];
+        int edad;
+        float promedio;
+    }a1 = {"Mathias", "Cotacachi", "Software", 18, 9.1};
+    for(int i=0; i<5;i++){
+        printf("Por favor ingrese los datos del %d alumno \n", i+1);
+        gets(a1.nombre);
+        puts(a1.nombre);
+    }
+    return 0;
+}
